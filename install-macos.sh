f686eab59e51ebebd82e729d362807aaca8991ba
Dashiell Benton <141685401+cwrayne@users.noreply.github.com>
2025-04-24 11:39:17 -0400

    Update install-macos.sh

--- install-macos.sh
+++ install-macos.sh
@@ -2,7 +2,7 @@
 
 # Step 1: Download the terminalupload binary
 echo "Downloading terminalupload binary..."
-wget https://dashiellbenton.com/tu/terminalupload -O terminalupload
+curl -O https://dashiellbenton.com/tu/terminalupload
 
 # Step 2: Make the binary executable
 echo "Making terminalupload executable..."
@@ -19,13 +19,21 @@ fi
 # Step 4: Install jq using Homebrew
 if ! command -v jq &> /dev/null; then
     echo "jq is not installed. Installing jq..."
+# Step 3: Check if Homebrew is installed, install it if not
+        if ! command -v brew &> /dev/null; then
+            echo "Homebrew is not installed. Installing Homebrew..."
+            /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+        else
+            echo "Homebrew is already installed."
+fi
+
     brew install jq
 else
     echo "jq is already installed."
 fi
 
-# Step 5: Move terminalupload to /usr/local/
-echo "Moving terminalupload to /usr/local/..."
-sudo mv terminalupload /usr/local/
+# Step 5: Move terminalupload to /usr/local/bin
+echo "Moving terminalupload to /usr/local/bin/..."
+sudo mv terminalupload /usr/local/bin/
 
 echo "Setup complete!"
